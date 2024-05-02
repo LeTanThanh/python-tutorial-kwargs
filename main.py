80f5f55c1c4345b9e52c1de835c21090adbc3648
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-02 17:33:46 +0700

    Introduction to the Python **kwargs parameters

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 if __name__ == "__main__":
-  print("Python **kwargs")
+  # Introduction to the Python **kwargs parameters
+
+  def connect(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+
+  connect()
+  connect(server = "localhost", port = 3306, user = "root", password = "Py1hon!Xt")
+
+  config = {
+    "server": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "Py1hon!Xt"
+  }
+  connect(**config)
+
+  def connect(fn, **kwargs):
+    print(kwargs)
